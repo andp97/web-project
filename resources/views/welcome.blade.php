7ec9ae0eb1f7d46b834cd9c1387886ce9b2f3406
Andrea Pavone <htcandrea@hotmail.it>
2017-08-07 22:28:54 +0200

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    LARAVEL TEST
                 </div>
 
                 <div class="links">
